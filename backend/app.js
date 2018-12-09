309caaba5778caf6fbaaea39373dfbb2da233303
Asfand <asfandnadeem116@gmail.com>
2018-12-09 16:22:55 +0500

    SDS

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose =require("mongoose");
+const path = require("path");
+
+const postsRoutes=require("./routes/posts");
+const userRoutes=require("./routes/user");
+
+const app = express();
+mongoose.connect("mongodb://asfand:Nj3atbLzmaV5WZHJ@comsatssocial-shard-00-00-y7oqn.mongodb.net:27017,comsatssocial-shard-00-01-y7oqn.mongodb.net:27017,comsatssocial-shard-00-02-y7oqn.mongodb.net:27017/socialdb?ssl=true&replicaSet=ComsatsSocial-shard-0&authSource=admin&retryWrites=true")
+  .then(()=>{
+    console.log('Connected to database');
+  })
+  .catch(error =>{
+    console.log(error);
+    console.log("Connection failed");
+  });
+// mongoose.connect("mongodb+srv://asfand:Nj3atbLzmaV5WZHJ@comsatssocial-y7oqn.mongodb.net/socialdb?retryWrites=true")
+//   .then(()=>{
+//     console.log('Connected to database');
+//   })
+//   .catch(error =>{
+//     console.log(error);
+//     console.log("Connection failed");
+//   });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+
+app.use("/api/posts",postsRoutes);
+app.use("/api/user",userRoutes);
+
+module.exports = app;
---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,101 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+import { Post } from './post.model';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Router} from '@angular/router';
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+  private posts: Post[] = [];
+  private postsUpdated = new Subject<{posts: Post[], postCount: number}>();
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+
+
+  getPosts(postsPerPage: number, currentPage: number) { // httpclientmodule
+    const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`; // `` backtips are for dynamically adding values into strings
+   this.http
+     .get<{message: string, posts: any, maxPosts: number}>(
+       'http://localhost:3000/api/posts'
+     )
+     .pipe(map((postData) => {
+       return { posts: postData.posts.map(post => {
+         return {
+           title: post.title,
+           content: post.content,
+           id: post._id,
+           creator: post.creator,
+           imagePath: post.imagePath
+         };
+       }), maxPosts: postData.maxPosts };
+    }))// change rterieving data
+     .subscribe(transformedPostData => {
+      this.posts = transformedPostData.posts;
+      this.postsUpdated.next({
+          posts: [...this.posts],
+          postCount: transformedPostData.maxPosts
+        }
+      );
+     }); // subscribe is to liosten
+  }
+
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
+  addPost(title: string, content: string , image: File) {
+    const postData =  new FormData();
+    postData.append('title', title);
+      postData.append('content', content);
+    postData.append('image', image, title);
+    this.http
+      .post<{ message: string, post: Post }>(
+        'http://localhost:3000/api/posts',
+        postData)
+      .subscribe( responseData  => {
+        this.router.navigate(['/messages']);
+      });
+  }
+
+  getPost(id: string) {
+    return this.http.get<{
+      _id: string,
+      title: string,
+      content: string,
+      creator: string,
+      imagePath: string
+    }>('http://localhost:3000/api/posts/' + id) ;
+  }
+
+  updatePost(id: string , title: string, content: string, image: File | string) {
+    let postData: Post |FormData ;
+    if (typeof(image) === 'object') {
+       postData = new FormData();
+       postData.append('id', id);
+      postData.append('title', title);
+      postData.append('content', content);
+      postData.append('image', image, title);
+    } else {
+       postData = {
+        id: id,
+        title: title,
+        content: content,
+         creator: null,
+        imagePath: image
+      };
+
+    }
+    this.http.put('http://localhost:3000/api/posts/' + id, postData)
+      .subscribe(response => {
+        this.router.navigate(['/messages']);
+      });
+  }
+
+  deletePost(postId: string) {
+   return this.http
+     .delete('http://localhost:3000/api/posts/' + postId);
+        }
+}
